a2b21e0f09d9fb8b7cfa7c2ffca2c6c99940610a
Rohit Singh <rkochhar@uwaterloo.ca>
2020-11-16 20:11:27 -0400

    Continued Dockerization, Fixed static file serving

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jrr)3=l19jc&=l-j^w18644-)bxau=1h*7-wczibi*q-kk5&qn'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 # Application definition
 
@@ -120,4 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
--- app/projects/models.py
+++ app/projects/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 class Project(models.Model):
     # Constants ----------------------------------------------------------------
+
+    # Fields -------------------------------------------------------------------
+    s_Title                 = models.CharField(max_length=100)
+    s_Description           = models.TextField()
+    s_ImageFile             = models.CharField(max_length=20, default = 'stock1.png')
+    s_LinkGitHub            = models.CharField(max_length=100, default = "None")
+
+    def __str__(self):
+        return f"{self.s_Title}"
+
+class TechnologyUsed(models.Model):
+    # Constants ----------------------------------------------------------------
     MATLAB  = 'MATLAB'
     CPP     = 'C++'
     PY      = 'Python'
@@ -16,18 +28,11 @@ class Project(models.Model):
                 (SL, "Simulink"),
                 (DOCKER, "Docker"),
                 ("None", "None"),
-
     ]
+
     # Fields -------------------------------------------------------------------
-    s_Title                 = models.CharField(max_length=100)
-    s_Description           = models.TextField()
-    s_PrimaryTechnology     = models.CharField(max_length=20, choices = TechnologyChoices, default = "None")
-    s_SecondaryTechnology   = models.CharField(max_length=20, choices = TechnologyChoices, default = "None")
-    s_TertiaryTechnology    = models.CharField(max_length=20, choices = TechnologyChoices, default = "None")
-    s_ImageFile             = models.CharField(max_length=20, default = 'stock1.png')
-    s_LinkGitHub            = models.CharField(max_length=100, default = "None")
-
+    s_Name          = models.CharField(max_length=50, choices = TechnologyChoices, default = "None")
+    i_Value         = models.IntegerField(default=0)
+    o_Project       = models.ManyToManyField('Project', related_name='technologies')
     def __str__(self):
-        return f"{self.s_Title}"
-
-class TechnologyUsed(models.Model):
+        return f"{self.s_Name}"
